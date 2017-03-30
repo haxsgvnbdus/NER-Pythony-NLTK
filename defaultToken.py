f9c8fbdf53b7109b0b9f1413cd8e3dd441585380
Han the Heo <Han the Heo>
2017-03-29 22:08:58 -0600

    fixed default tokenizer module

---
+++ defaultToken.py
@@ -0,0 +1,25 @@
+import tokenize
+import sys, getopt, math, os.path
+
+
+def main(argv):
+
+	try:
+		opts, args = getopt.getopt(argv,"f:")
+    	except getopt.GetoptError:
+        	sys.exit(2)	
+
+    	for opt, arg in opts:
+    		if opt == '-f':
+        		if os.path.isfile(arg):
+				input = open(arg, 'r')
+				tokens = tokenize.tokenize(input.readline, handle_token)
+				
+	
+def handle_token(type, token, (srow, scol), (erow, ecol), line):
+
+	print "%d-%d:%s" % (scol, ecol, token)		#repr for tokens to see its type
+	
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
