3b7d2f28df70b538d18fb9e979c1800e5a2aae00
Han the Heo <Han the Heo>
2017-03-29 22:12:57 -0600

    fixed bug

--- customToken.py
+++ customToken.py
@@ -1,5 +1,7 @@
+#!/usr/bin/ python
+# -*- coding: utf-8 -*-
 import re
-import sys, getopt, math, os.path
+import sys, getopt 
 
 
 def main(argv):
@@ -7,34 +9,48 @@ def main(argv):
 	opts, args = getopt.getopt(argv,"f:")    	
     	for opt, arg in opts:
     		if opt == '-f':
-			print "tokens file: " + arg 
 			customize(open(arg, 'r'))
 				
 				
 def customize(file):
 	lines =  file.read().splitlines()
 	isFirstLine = True
-	
+	pre_line = 0
 	for each in lines:
-		matches = re.match(r'(\d+)-(\d+):(.*)', each)
-		if matches is not None:		 
-			s = int(matches.group(1))										
-		
+		matches = re.match(r'(\d+),(\d+)-(\d+),(\d+):(.*)', each)
+		if matches is not None:	 
+			s = int(matches.group(2))										
+			cur_line = int(matches.group(1))
+			
+			#this crap solves the several space crap 
+			if cur_line > pre_line and s is not 0:
+				e = int(matches.group(4))
+				new_s = new_e
+				new_e += e
+				
+			#3 cases 
 			if not isFirstLine: 
-				if int(matches.group(1)) == 0:
-					e = int(matches.group(2))
+				if int(matches.group(2)) == 0:
+					e = int(matches.group(4))
 					new_s = new_e
 					new_e += e
 				else:
 					new_s = s - e + new_e 
-					e = int(matches.group(2))
+					e = int(matches.group(4))
 					new_e = e - s + new_s 
 											
 			else:	
 				new_s = s		
-				e = new_e = int(matches.group(2)) 
+				e = new_e = int(matches.group(4)) 
+			
+			#cleaning output: if this is words, print
+			w = re.match(r'(.{1}\d+|\w+)', matches.group(5))
+			if w is not None:
+				fprint(matches.group(5), new_s, new_e)
+			
+			pre_line = int(matches.group(1))																
+		
 
-			fprint(matches.group(3), new_s, new_e)																
 		isFirstLine = False
 
 
